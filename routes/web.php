4eaa77a235ba4b0bb8c8f4d196cc7205800c0ebc
Gleb Tushin <g.tushin2016@gmail.com>
2024-03-21 12:03:35 +0300

    TimezoneController add. DateController turned to invokable controller.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DateController;
+use App\Http\Controllers\TimezoneController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('/date',[DateController::class,'__invoke']);
+route::get('/date',DateController::class);
+route::get('/timezone',[TimezoneController::class,'get_timezone']);
